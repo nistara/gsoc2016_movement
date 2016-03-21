da9126789746546034608b769c71936df22e62e9
Nistara <nistara@users.noreply.github.com>
2016-03-20 21:27:21 -0700

    Update gsoc_nistara.R

--- gsoc_nistara.R
+++ gsoc_nistara.R
@@ -33,7 +33,7 @@ points(subfires, col = "green")
 
 
 # =========================================================================
-# Test: Build SQL function that select the points
+# Test: Build SQL function that selects the points
 # within a spatio-temporal window
 # =========================================================================
 
